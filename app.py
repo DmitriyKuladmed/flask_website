ebe4f1c3cda443137f6ef5de810ef9581801119d
Asus <dima.kulaga5@gmail.com>
2022-09-27 11:28:06 +0300

    start_files

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+import templates
+
 
 app = Flask(__name__)
 
@@ -10,6 +13,10 @@ def index():
 @app.route('/about')
 def about():
     return 'About Us'
+
+@app.route('/myPage')
+def myPage():
+    return render_template("index.html")
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
