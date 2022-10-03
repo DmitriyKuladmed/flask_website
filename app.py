db88bbfd52e2ee506ed30e619dfb29a869471807
Asus <dima.kulaga5@gmail.com>
2022-10-03 13:03:29 +0300

    changed main.css, index.html and base.html; added blog.db

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-import templates
+from flask_sqlalchemy import *
+from datetime import datetime
+from config import Config
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(64), unique=True)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.String, nullable=False)
+    data = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' %self.id
 
 @app.route('/')
 @app.route('/home')
